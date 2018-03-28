abb222f755c75e4f352319e51b94f36610451b84
baoyudaiyu <36811012+baoyudaiyu@users.noreply.github.com>
2018-03-28 10:24:42 +0800

    更新商品发布1.2

--- application/store/controller/Application.php
+++ application/store/controller/Application.php
@@ -2,6 +2,9 @@
 namespace app\store\controller;
 use \think\Controller;
 use \think\db;
+use \think\Session;
+use \think\Loader;
+use think\Cookie;
 class Application extends Controller
 {
     /**
@@ -13,6 +16,19 @@ class Application extends Controller
      **/
     public function storeEnter()
     {
+//        根据IP定位用户所在地区设置默认地址
+
+
+
+
+        //消除手机号记录
+        if(Session::has('mobile')){
+            Session::delete('mobile');
+        }
+
+        Session::set('send_code',$this->random(6,1));
+        $send_code=Session::get('send_code');
+        $this->assign("send_code",$send_code);
 //        查询省
         $where=["pid"=>0];
         $re=db("hy_area")->where($where)->select();
@@ -22,12 +38,140 @@ class Application extends Controller
         $re=db("hy_area")->where($where)->select();
         $re=json_encode($re);
         $this->assign("city",$re);
-        return $this->fetch();
+        return $this->fetch("Application/storeEnter");
     }
+    /**
+     *  *  功能描述：查询城市
+     *  参数：id
+     *  返回：查询到的城市数组
+     *  作者:qingtian Y
+     *  时间：18-3-27
+     **/
     public function getCity(){
         $id=input('?post.id')?input('id'):1;
         $where=["pid"=>$id];
         $re=db("hy_area")->where($where)->select();
         echo json_encode($re);
+    }
+    /**
+     *  *  功能描述：点击获取短信获取登录验证码
+     *  参数：无
+     *  返回：无
+     *  作者:qingtian Y
+     *  时间：18-3-27
+     **/
+    public function getCode(){
+// 引入 extend/qrcode.php
+        //获取手机号
+        $mobile=input('?post.mobile')?input('mobile'):"";
+        $send_code=input('?post.send_code')?input('send_code'):"";
+        $where=["store_phone"=>$mobile];
+        $re=db("store_info")->where($where)->select();
+        Loader::import('storeSms', EXTEND_PATH);
+//// 助手函数
+        import('storeSms', EXTEND_PATH);
+        new \StoreSms($re,$mobile,$send_code);
+        //查询手机号是否已被注册
+
+    }
+    /**
+     *  *  功能描述：商家申请入驻表单提交
+     *  参数：无
+     *  返回：无
+     *  作者:qingtian Y
+     *  时间：18-3-27
+     **/
+    public function submitform(){
+        if($_POST){
+            //echo '<pre>';print_r($_POST);print_r($_SESSION);
+            $code=input('?post.code')?input('code'):"";
+            $settledType=input('?post.settledType')?input('settledType'):"";
+            $corporation=input('?post.corporation')?input('corporation'):"";
+            $corporation_province=input('?post.corporation_province')?input('corporation_province'):"";
+            $corporation_city=input('?post.corporation_city')?input('corporation_city'):"";
+            $corporation_town=input('?post.corporation_town')?input('corporation_town'):"";
+            $corporation_detail=input('?post.corporation_detail')?input('corporation_detail'):"";
+            $mobile=input('?post.mobile')?input('mobile'):"";
+            $email=input('?post.email')?input('email'):"";
+            //后台验证
+            if(empty($code)){
+                $this->error('验证码不能为空');
+            }
+            if($settledType!="景点入驻" && $settledType!="酒店入驻"){
+                $this->error('非法请求');
+            }
+            if(empty($corporation)||count($corporation)>50){
+                $this->error('非法请求');
+            }
+            if(empty($corporation_province)||!preg_match("/^\d*$/",$corporation_province)){
+                $this->error('非法请求3');
+            }
+            if(empty($corporation_city)||!preg_match("/^\d*$/",$corporation_city)){
+                $this->error('非法请求4');
+            }
+            if(empty($corporation_town)||!preg_match("/^\d*$/",$corporation_town)){
+                $this->error('非法请求5');
+            }
+            if(empty($corporation_detail)||count($corporation_detail)>50){
+                $this->error('非法请求6');
+            }
+            if(empty($email)||!preg_match("/^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/",$email)){
+                $this->error('非法请求7');
+            }
+            if(empty($mobile)||!preg_match("/^(0|86|17951)?1[0-9]{10}/",$mobile)){
+                $this->error('非法请求8');
+            }
+            $mobile=input('?post.mobile')?input('mobile'):"";
+            $mobile_code=input('?post.mobile_code')?input('mobile_code'):"";
+            //验证码是否能通过验证
+            if($mobile!=Session::get('mobile') or $mobile_code!=Session::get('mobile_code') or empty($_POST['mobile']) or empty($_POST['mobile_code'])){
+                // cookie初始化
+                Cookie::init(['prefix'=>'travel_','expire'=>3600,'path'=>'/']);
+                // 设置
+                cookie('code_error', "验证码错误",50);
+
+                //消除手机号记录
+                if(Session::has('mobile')){
+                    Session::delete('mobile');
+                }
+                    Session::set('send_code',$this->random(6,1));
+                    $send_code=Session::get('send_code');
+                    $this->assign("send_code",$send_code);
+                //        查询省
+                    $where=["pid"=>0];
+                    $re=db("hy_area")->where($where)->select();
+                    $re=json_encode($re);
+                    $this->assign("province",$re);
+                    $where=["pid"=>1];
+                    $re=db("hy_area")->where($where)->select();
+                    $re=json_encode($re);
+                    $this->assign("city",$re);
+                    $this->redirect('Application/storeEnter');
+            }else{
+                $submitTime=@date("Y-m-d H:i:s",time()+60*60*8);
+                Db::table('store_info')
+                    ->data(['store_name'=>$corporation,'store_state'=>"F","store_phone"=>$mobile,"store_apply_time"=>$submitTime])
+                    ->insert();
+                Session::set("mobile",'');
+                Session::set("mobile_code",'');
+                $this->success('提交成功！请保持手机畅通。');
+            }
+        }else{
+            $this->error('非法请求！');
+        }
+    }
+    private  function random($length = 6 , $numeric = 0) {
+        PHP_VERSION < '4.2.0' && mt_srand((double)microtime() * 1000000);
+        if($numeric) {
+            $hash = sprintf('%0'.$length.'d', mt_rand(0, pow(10, $length) - 1));
+        } else {
+            $hash = '';
+            $chars = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789abcdefghjkmnpqrstuvwxyz';
+            $max = strlen($chars) - 1;
+            for($i = 0; $i < $length; $i++) {
+                $hash .= $chars[mt_rand(0, $max)];
+            }
+        }
+        return $hash;
     }
 }
--- public/static/js/storeEnter.js
+++ public/static/js/storeEnter.js
@@ -1,4 +1,65 @@
-console.log(province);
+/**
+ *  *  功能描述：获取COOKIE
+ *  参数：cname
+ *  返回：content
+ *  作者:qingtian Y
+ *  时间：18-3-27
+ **/
+function getCookie(cname)
+{
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++)
+    {
+        var c = ca[i].trim();
+        if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+    }
+    return "";
+}
+
+
+
+
+
+
+
+
+
+var iTime = 59;
+var Account;
+function RemainTime(){
+    document.getElementById('zphone').disabled = true;
+    var iSecond,sSecond="",sTime="";
+    if (iTime >= 0){
+        iSecond = parseInt(iTime%60);
+        iMinute = parseInt(iTime/60);
+        if (iSecond >= 0){
+            if(iMinute>0){
+                sSecond = iMinute + "分" + iSecond + "秒";
+            }else{
+                sSecond = iSecond + "秒";
+            }
+        }
+        sTime=sSecond;
+        if(iTime==0){
+            clearTimeout(Account);
+            sTime='获取手机验证码';
+            iTime = 59;
+            document.getElementById('zphone').disabled = false;
+        }else{
+            Account = setTimeout("RemainTime()",1000);
+            iTime=iTime-1;
+        }
+    }else{
+        sTime='没有倒计时';
+    }
+    document.getElementById('zphone').value = sTime;
+}
+
+
+
+
+
 var vue=new Vue({
     el:'#myapp',
     data:{
@@ -7,23 +68,27 @@ var vue=new Vue({
         townShow:false,
         province:province,
         city:city,
-        town:[]
+        mobile:"",
+        send_code:send_code,
+        town:[],
+        required:false,
+        tip_content:""
     },
+
     methods:{
         selectShowNoneFun:function () {
             this.selectShowNone=false;
-
         },
         getCity:function (e) {
             $.post($getCityUrl,{"id":e.target.value},function (res) {
                 var cityArr=JSON.parse(res);
                 this.city=cityArr;
+                this.required=true;
                 if(cityArr.length!=1){
                     this.cityShow=true;
                 }else {
                     this.cityShow=false;
                 }
-
                 var pid=cityArr[0].id;
                 $.post($getCityUrl,{"id":pid},function (res) {
                     var townArr=JSON.parse(res);
@@ -38,6 +103,40 @@ var vue=new Vue({
                 this.town=townArr;
                 this.townShow=true;
             }.bind(this))
+        },
+        //获取短信验证码
+        getCode:function () {
+            $.post($getCodeUrl,{"mobile":this.mobile,"send_code":this.send_code},function (msg) {
+                if(msg=='提交成功'){
+                    RemainTime();
+                }else {
+                    this.tip_content=msg;
+                    $('#myModal').modal('show') ;
+                }
+            }.bind(this))
+        },
+        /**
+         *  *  功能描述：打开页面时先判断COOKIE中有没错误提示，做出对应显示
+         *  参数：无
+         *  返回：无
+         *  作者:qingtian Y
+         *  时间：18-3-27
+         **/
+        showCodeTips:function () {
+            var travel_code_error=getCookie("travel_code_error");
+            if(travel_code_error!=""){
+                //模态框弹出警告
+                this.tip_content=decodeURI(travel_code_error);
+                $('#myModal').modal('show') ;
+                // document.cookie = "travel_code_error=; expires=Thu, 01 Jan 1970 00:00:00 GMT";
+            }
         }
+
+    },
+    mounted: function () {
+        this.showCodeTips();
+    },
+    beforeUpdate: function () {
+
     }
 });
