3a4e5d5886e57b921523e9dcbdae7f7b15e93932
baoyudaiyu <36811012+baoyudaiyu@users.noreply.github.com>
2018-04-04 09:44:57 +0800

    杨庆畑/4.4

--- application/storemanage/controller/Index.php
+++ application/storemanage/controller/Index.php
@@ -96,9 +96,50 @@ class Index extends Controller
             // 返回未登录信息
             $msg->returnJson("loginMsg","UPDATA_ERROR",$data) ;
         }
-
-
-
     }
+    /**
+     *  功能描述:获取商家信息
+     *  参数：无
+     *  返回：无
+     *  作者:qingtian Y
+     *  时间：18-4-3
+     **/
+    public function getinfo(){
 
+        // 如果单前页面不在登录页面，则为N 否则为Y
+        $url =  strcmp(strstr($_SERVER['HTTP_REFERER'],"/login/index.html"),"/login/index.html")==0 ? "Y":"N";
+
+        // 查询用户是否登录
+        $msg = new \user() ;
+        if( Session::has('loginData') )
+        {
+            $res = unserialize(Session::get('loginData')) ;
+            if( strcmp($res['userType'],'store')==0 )
+            {
+                $data = [
+                    "name" =>  $res['store_name'] ,
+                    "store_phone" =>  $res['store_phone'] ,
+
+                ] ;
+                // 返回用户信息
+                $msg->returnJson("loginMsg","SUCCESS",$data,10000) ;
+            }
+            else
+            {
+
+                // 返回未登录信息
+                $msg->returnJson("loginMsg","UPDATA_ERROR") ;
+
+            }
+        }
+        else
+        {
+            $data = [
+                "setJump" => $url
+            ] ;
+            // 返回用户信息
+            // 返回未登录信息
+            $msg->returnJson("loginMsg","UPDATA_ERROR",$data) ;
+        }
+    }
 }
