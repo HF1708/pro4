e39ab309717c6fbfee6898018139ed4992cbc3f3
baoyudaiyu <36811012+baoyudaiyu@users.noreply.github.com>
2018-04-09 16:49:32 +0800

    更新小程序

--- public/static/js/nav_platform.js
+++ public/static/js/nav_platform.js
@@ -9,11 +9,6 @@ var navs = [{
 	"href" : AdvertUrl,
 	"spread" : false
 },{
-    "title" : "发布商品",
-    "icon" : "icon-jlk",
-    "href" : "",
-    "spread" : false
-},{
     "title" : "酒店管理",
     "icon" : "icon-text",
     "href" : HotelbgUrl,
@@ -43,34 +38,9 @@ var navs = [{
     "icon" : "icon-text",
     "href" : NotesUrl,
     "spread" : false
-},
-	{
-	"title" : "404页面",
-	"icon" : "&#xe61c;",
-	"href" : "page/404.html",
-	"spread" : false
 },{
 	"title" : "客服聊天",
 	"icon" : "&#xe631;",
 	"href" : ServerChatUrl,
 	"spread" : false
-},{
-	"title" : "二级菜单演示",
-	"icon" : "&#xe61c;",
-	"href" : "",
-	"spread" : false,
-	"children" : [
-		{
-			"title" : "二级菜单1",
-			"icon" : "&#xe631;",
-			"href" : "",
-			"spread" : false
-		},
-		{
-			"title" : "二级菜单2",
-			"icon" : "&#xe631;",
-			"href" : "",
-			"spread" : false
-		}
-	]
 }]
