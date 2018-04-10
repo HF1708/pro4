2751b70c5ba1731c1a75690734a6c8d11e1efd94
qq457522114 <37685895+qq457522114@users.noreply.github.com>
2018-04-10 10:08:03 +0800

    凌永锦/4/10/更新 /1.0/1.930

---
+++ application/data/controller/Data.php
@@ -0,0 +1,32 @@
+<?php
+namespace app\data\controller;
+use think\Controller;
+use think\captcha ;
+use \think\Session ;
+
+class Data extends Controller
+{
+
+    /**
+     * 功能描述：获取推荐
+     * 参数：
+     * QQUser：
+     * 返回：推荐数据
+     * 作者：yonjin L
+     * 时间：18-4-10
+     */
+
+    /**
+     * 功能描述：获取
+     * 参数：
+     * QQUser：
+     * 返回：推荐数据
+     * 作者：yonjin L
+     * 时间：18-4-10
+     */
+
+}
+
+
+
+
